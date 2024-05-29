4eba40a527da1b727e30938c545a57c667c71e05
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-05-29 19:47:33 +0800

    [JS-3] Print 1 to given number but print fizz if multiples 3, buzz if 5. FizzBuzz if multiples of 3 and 5

--- week-1/javascript/3.js
+++ week-1/javascript/3.js
@@ -5,3 +5,19 @@
  * For numbers which are multiples of both three and five print 'FizzBuzz'.
  * @param {number} n The number to print.
  */
+
+function FizzBuzz(number){
+    for (var i = 1; i <= number; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 == 0) {
+            console.log("Fizz");
+        } else if (i % 5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+FizzBuzz(20);
