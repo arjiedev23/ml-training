dd82e8564b8e0996361cf301828acca06196a785
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-05-30 14:43:47 +0800

    [JS-7] Check Number custom error

--- week-1/javascript/7.js
+++ week-1/javascript/7.js
@@ -3,3 +3,17 @@
  * @param {number} num The number to check.
  * @throws {Error} If the number is not an integer.
  */
+
+function getNumber(num){
+    try {
+        if(typeof number !== "number"){
+            throw new Error(`${num} is not an integer.`);
+        } else {
+            return `${num} is an integer.`
+        }
+    } catch(ex) {
+        return ex.message;
+    }
+}
+
+console.log(getNumber("Sample"));
