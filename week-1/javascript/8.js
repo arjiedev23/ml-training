0906d73468c6e85dd28309c0c253d4ad209c2145
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-05-30 14:50:21 +0800

    [JS-8] Return all vowels in a string

--- week-1/javascript/8.js
+++ week-1/javascript/8.js
@@ -1,5 +1,12 @@
 /**
  * Write a function that returns all vowels in a string.
  * @param {string} str The string to check.
- * @return {string} The string with all vowels removed.
+ * @return {string} The string with all vowels.
  */
+
+function checkVowel(str) {
+    let result = str.match(/[aeiou]/ig).join("");
+    return result;
+}
+
+console.log(checkVowel("Arjie"));
