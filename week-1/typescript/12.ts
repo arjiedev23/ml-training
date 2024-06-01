d3aae5e53e01136e6a4268969cbf7a225c43f241
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-06-01 20:18:56 +0800

    [TS-12] Custom error - Check if array is empty

--- week-1/typescript/12.ts
+++ week-1/typescript/12.ts
@@ -3,3 +3,18 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+
+var number : string[] = [];
+function checkArray(arr : any){
+    try {
+        if (arr.length === 0) {
+            throw new Error("Array is empty");
+        } else {
+            return "Array is not empty";
+        }
+    } catch (ex: any) {
+        return ex.message;
+    }
+}
+
+console.log(checkArray(number));
