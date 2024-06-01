fa055ebd39076d63c1c3d8ac1c30273c1a602212
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-06-01 16:57:36 +0800

    [JS-14] Get the lowest and the highest number in the array

--- week-1/javascript/14.js
+++ week-1/javascript/14.js
@@ -6,3 +6,13 @@
  * Example array: [5, 2, 8, 1, 9, 3]
  * Expected output: "The lowest number is 1 and the highest number is 9."
  */
+let number = [5, 2, 8, 1, 9, 3];
+
+function LowestHighest(arr) {
+    const high = Math.max(...arr);
+    const low = Math.min(...arr);
+
+    return `The lowest number is ${low} and the highest number is ${high}`;
+}
+
+console.log(LowestHighest(number));
