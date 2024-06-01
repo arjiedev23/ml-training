ca0be958ef22a6abfb33b8283ec7ad11ad33466d
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-06-01 20:05:27 +0800

    [TS-10] Custom error - check second number is zero

--- week-1/typescript/10.ts
+++ week-1/typescript/10.ts
@@ -4,3 +4,17 @@
  * @param {number} num2 The second number to check.
  * @throws {Error} If the second number is 0.
  */
+
+function activity10(num1 : number, num2 : number){
+    try {
+        if (num2 === 0) {
+            throw new Error("Num2 is 0");
+        } else {
+            return "Num2 is not 0";
+        }
+    } catch (err : any) {
+        return err.message;
+    }
+}
+
+console.log(activity10(1,1));
