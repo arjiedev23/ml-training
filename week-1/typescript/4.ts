fe0c247f002e0e68a21095c163043739a95ce53d
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-06-01 18:49:15 +0800

    [TS-4] Current datetime format

--- week-1/typescript/4.ts
+++ week-1/typescript/4.ts
@@ -3,3 +3,12 @@
  * 'MM-DD-YYYY HH:MM:SS'.
  * @return {string} The current date and time in the format 'MM-DD-YYYY HH:MM:SS'.
  */
+
+function DisplayCurrentDate(){
+    var currentdate = new Date(); 
+    var datetime = String(currentdate.getMonth()+1).padStart(2, '0') + "-" + String(currentdate.getDate()).padStart(2, '0') + "-" + currentdate.getFullYear() + " "  
+                + String(currentdate.getHours()).padStart(2, '0') + ":" + String(currentdate.getMinutes()).padStart(2, '0') + ":" + String(currentdate.getSeconds()).padStart(2, '0');
+    console.log(datetime);
+}
+
+DisplayCurrentDate();
