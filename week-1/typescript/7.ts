b0d89bb59db6332a4c8e4df07b682496f0a86eb3
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-06-01 19:55:58 +0800

    [TS-7] Check if parameter is not a integer customer error

--- week-1/typescript/7.ts
+++ week-1/typescript/7.ts
@@ -3,3 +3,17 @@
  * @param {number} num The number to check.
  * @throws {Error} If the number is not an integer.
  */
+
+function checkNumber(num : any) : string {
+    try {
+        if(typeof num !== "number"){
+            throw new Error(`${num} is not an integer.`);
+        } else {
+            return `${num} is an integer.`
+        }
+    } catch(ex : any) {
+        return ex.message;
+    }
+}
+
+console.log(checkNumber(9));
