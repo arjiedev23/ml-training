38899a8b6c0dbc78c097e5380f50ac2efed5d0a6
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-06-01 18:46:36 +0800

    [TS-2] Sum all number in the array

--- week-1/typescript/2.ts
+++ week-1/typescript/2.ts
@@ -3,3 +3,12 @@
  * @param {number[]} arr The array of numbers.
  * @return {number} The sum of all numbers in the array.
  */
+
+var arr: number[] = [1,2,3,4];
+
+function getSum(a : number,b : number,c : number,d: number) : number{
+    let sum = 0;
+    return a + b + c + d;
+}
+
+console.log(getSum(arr[0], arr[1], arr[2], arr[3]));
