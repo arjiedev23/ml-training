8b260695d5e8a5db31523014f05c336dd6bf78a8
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-06-01 19:51:29 +0800

    [TS-5] Temperature Fahrenheit

--- week-1/typescript/5.ts
+++ week-1/typescript/5.ts
@@ -4,3 +4,10 @@
  * @param {number} celsius The temperature in Celsius.
  * @return {number} The temperature in Fahrenheit.
  */
+
+function ConvertDF(celsius : number){
+    var convert = (celsius * 9) / 5 + 32;
+    return `${celsius}°C = ${convert}°F`;
+}
+
+console.log(ConvertDF(60));
