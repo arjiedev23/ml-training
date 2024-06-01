0ce7b940fa68f0358496eefec95a4d840561e95d
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-06-01 20:28:42 +0800

    [TS-17] Validate password

--- week-1/typescript/17.ts
+++ week-1/typescript/17.ts
@@ -19,3 +19,31 @@
  * @param {string} password
  * @returns {string}
  */
+
+function HasUpperCase(str : string) {
+    return str !== str.toLowerCase();
+}
+
+function HasLowerCase(str: string) {
+    return str.toUpperCase() != str;
+}
+
+function ValidatePass (password : string) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    const numberChars = /\d/;
+    if (password.length <= 8) {
+        return "Password must be at least 8 characters long";
+    } else if (!HasUpperCase(password)) {
+        return "Password must contain at least one uppercase letter";
+    } else if (!HasLowerCase(password)) {
+        return "Password must contain at least one lowercase letter";
+    } else if (!numberChars.test(password)) {
+        return "Password must contain at least one number";
+    } else if (!specialChars.test(password)) {
+        return "Password must contain at least one special character";
+    } else {
+        return "Password is valid";
+    }
+}
+
+console.log(ValidatePass("sssssS123@"));
