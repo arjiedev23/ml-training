55f94f5bbbbd122a8863f86c701a37d1a13bba6e
Arjie Niño Wenceslao <arjienino.wenceslao@mlhuillier.com>
2024-06-01 16:48:10 +0800

    [JS-12] check if array is empty

--- week-1/javascript/12.js
+++ week-1/javascript/12.js
@@ -3,3 +3,18 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+
+var number = [];
+function checkArr(arr){
+    try {
+        if (arr.length === 0) {
+            throw new Error("Array is empty");
+        } else {
+            return "Array is not empty";
+        }
+    } catch (ex) {
+        return ex.message;
+    }
+}
+
+console.log(checkArr(number));
